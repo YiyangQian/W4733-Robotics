bd9eb03a1f4a2651fbde84213f19d111ec49cd12
Ziqing Chen <zc2388@columbia.edu>
2018-10-23 17:54:56 -0400

    p2 cleaning

--- lab3/src/p2.py
+++ lab3/src/p2.py
@@ -40,19 +40,7 @@ class MoveBaseSquare():
         
         # How big is the square we want the robot to navigate?
         square_size = rospy.get_param("~square_size", 1.0) # meters
-        
-        # Create a list to hold the target quaternions (orientations)
-        quaternions = list()
-        
-        # First define the corner orientations as Euler angles
-        euler_angles = (pi/2, pi, 3*pi/2, 0)
-        
-        # Then convert the angles to quaternions
-        for angle in euler_angles:
-            q_angle = quaternion_from_euler(0, 0, angle, axes='sxyz')
-            q = Quaternion(*q_angle)
-            quaternions.append(q)
-        
+
         # Create a list to hold the waypoint poses
         waypoints = list()
         
@@ -67,19 +55,13 @@ class MoveBaseSquare():
             for j in range(current_convex_hull.shape[0]):
                 new_x = current_convex_hull[j][0]/100
                 new_y = current_convex_hull[j][1]/100
-                this_convex_waypoints.append(Pose(Point( new_x, new_y , 0.0), quaternions[0]))
+                this_convex_waypoints.append(Point(new_x, new_y , 0.0))
             waypoints.append(this_convex_waypoints)
 
         # Initialize the visualization markers for RViz
 
         self.init_markers(total_length)
-        
-        point_1 = Pose(Point(square_size, 0.0, 0.0), quaternions[0])
-        print(point_1)
 
-        # Set a visualization marker at each waypoint        
-        
-            
         # Publisher to manually control the robot (e.g. to stop it, queue_size=5)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=5)
         
@@ -91,27 +73,17 @@ class MoveBaseSquare():
         # Wait 60 seconds for the action server to become available
         self.move_base.wait_for_server(rospy.Duration(60))
         
-        rospy.loginfo("Connected to move base server")
-        rospy.loginfo("Starting navigation test")
-        
-        cnt = 0
         for i in range(len(convex_hull_array)):
             #self.markers.points = list()
             for j in range(len(waypoints[i]) -1 ):           
 
-                p = waypoints[i][j].position
-                q = waypoints[i][j+1].position
-
-                self.marker_array.markers[i].points.append(p)
-                self.marker_array.markers[i].points.append(q)
-                self.marker_pub.publish(self.marker_array.markers[i])
-                cnt+=1
+                p = waypoints[i][j]
+                q = waypoints[i][j+1]
+                self.publish_one_edge(p,q)
+                
                 rospy.sleep(0.1)
 
-            self.marker_array.markers[i].points.append(waypoints[i][-1].position)
-            self.marker_array.markers[i].points.append(waypoints[i][0].position)
-            self.marker_pub.publish(self.marker_array.markers[i])
-            cnt+=1
+            self.publish_one_edge(waypoints[i][-1],waypoints[i][0])
             rospy.sleep(0.1)
 
         
@@ -119,35 +91,41 @@ class MoveBaseSquare():
         # Set up our waypoint markers
         # Define a marker publisher.
         self.marker_array = MarkerArray()
-        self.marker_pub = rospy.Publisher('vgraph_markers', Marker, queue_size=5)
+        self.marker_pub = rospy.Publisher('vgraph_markers', Marker, queue_size=20)
+        self.marker_id = 0
 
+    def publish_one_edge(self,p1,p2):
         marker_scale = 0.015
         marker_lifetime = 0 # 0 is forever
         marker_ns = 'vgraph'
-        marker_id = 0
+        
         marker_color = {'r': 0, 'g': 0, 'b': 1, 'a': 1.0}
         
+        markers = Marker()
+        markers.ns = marker_ns
+        markers.id = self.marker_id
+        markers.type = Marker.LINE_STRIP
+        markers.action = Marker.ADD
+        markers.lifetime = rospy.Duration(marker_lifetime)
+        markers.scale.x = marker_scale
+        markers.scale.y = marker_scale
+        markers.color.r = marker_color['r']
+        markers.color.g = marker_color['g']
+        markers.color.b = marker_color['b']
+        markers.color.a = marker_color['a']
+        
+        markers.header.frame_id = 'odom'
+        markers.header.stamp = rospy.Time.now()
+        markers.points = list()
+        markers.points.append(Point(p1.x,p1.y,0))
+        markers.points.append(Point(p2.x,p2.y,0))
 
-        for i in range(convex_size):
-            # Initialize the marker points list.
-            markers = Marker()
-            markers.ns = marker_ns
-            markers.id = marker_id
-            markers.type = Marker.LINE_STRIP
-            markers.action = Marker.ADD
-            markers.lifetime = rospy.Duration(marker_lifetime)
-            markers.scale.x = marker_scale
-            markers.scale.y = marker_scale
-            markers.color.r = marker_color['r']
-            markers.color.g = marker_color['g']
-            markers.color.b = marker_color['b']
-            markers.color.a = marker_color['a']
-            
-            markers.header.frame_id = 'odom'
-            markers.header.stamp = rospy.Time.now()
-            markers.points = list()
-            marker_id +=1
-            self.marker_array.markers.append(markers)
+
+        self.marker_array.markers.append(markers)
+        self.marker_pub.publish(self.marker_array.markers[-1])
+        self.marker_id +=1
+        
+
 
     def shutdown(self):
         rospy.loginfo("Stopping the robot...")
